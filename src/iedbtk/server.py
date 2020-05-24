008303d355c5745bc8c790c849a792c2482f604b
James A. Overton <james@overton.ca>
2020-05-24 12:18:25 -0400

    View references

--- src/iedbtk/server.py
+++ src/iedbtk/server.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
+import sqlite3
 import subprocess
 
-from flask import Flask, request, redirect, Response
+from flask import Flask, request, redirect, Response, render_template
 from requests import get, post
 import tsv2rdf
 
 #root = "/browse/"
 root = "/"
 data = tsv2rdf.readdir("data2")
+sqlite = "file:build/iedb.db?mode=ro"
 app = Flask(__name__, instance_relative_config=True)
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/favicon.ico')
 def favicon():
@@ -43,11 +51,60 @@ def style():
 
 @app.route('/')
 def index():
-    output = []
-    for tree in sorted(data["trees"]):
-        output.append(f"""<li><a href="{root}{tree}">{tree}</a>""")
-    output = "\n".join(output)
-    return f"<html><ul>{output}</ul></html>"
+    html = ["div",
+            ["p", ["a", {"href": "/reference/"}, "References"]],
+            ["p", ["a", {"href": "/sqlite-web/"}, "SQL Browser"]]]
+    return render_template("base.jinja2", html=tsv2rdf.render(html))
+
+
+@app.route('/reference/')
+def references():
+    with sqlite3.connect(sqlite, uri=True) as conn:
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+        cur.execute("""SELECT r.reference_id, a.article_title
+          FROM reference r
+          LEFT JOIN article a ON a.reference_id = r.reference_id
+          ORDER BY r.reference_id""")
+        items = []
+        for row in cur:
+            items.append(["li",
+                ["a",
+                 {"href": row["reference_id"]},
+                 row["reference_id"]],
+                " " + (row["article_title"] or "Submission")])
+        html = ["div",
+                ["h2", "References"],
+                ["ul"] + items]
+        return render_template("base.jinja2", html=tsv2rdf.render(html))
+
+
+@app.route('/reference/<reference_id>')
+def reference(reference_id):
+    with sqlite3.connect(sqlite, uri=True) as conn:
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+        html = ["div", ["h2", ["a", {"href": "./"}, "References"]]]
+
+        cur.execute(f"SELECT * FROM reference WHERE reference_id = '{reference_id}'")
+        rows = cur.fetchall()
+        items = []
+        for key, value in rows[0].items():
+            items.append(["li", ["strong", key], ": ", value])
+        items = ["ul", {"style": "list-style: none"}] + items
+        html.append(["h3", f"Reference {reference_id}"])
+        html.append(items)
+
+        cur.execute(f"SELECT * FROM article WHERE reference_id = '{reference_id}'")
+        rows = cur.fetchall()
+        items = []
+        for key, value in rows[0].items():
+            items.append(["li", ["strong", key], ": ", value])
+        items = ["ul", {"style": "list-style: none"}] + items
+        html.append(["h3", f"Article"])
+        html.append(items)
+
+        return render_template("base.jinja2", html=tsv2rdf.render(html))
 
 
 # Proxy /sqlite-web to localhost 8080
